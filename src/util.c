107b21e4c5e4033922db0dd2f6857fe130b40cec
Thomas BEGUIN <tbeguin@e1r8p20.42.fr>
2019-01-06 16:25:10 +0100

    Fin fonction util

--- src/util.c
+++ src/util.c
@@ -6,7 +6,7 @@
 /*   By: tbeguin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/06 14:36:52 by tbeguin           #+#    #+#             */
-/*   Updated: 2019/01/06 15:18:10 by tbeguin          ###   ########.fr       */
+/*   Updated: 2019/01/06 16:24:29 by tbeguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,37 +21,42 @@ void	ft_free_list(t_list *alst)
 	{
 		next = alst->next;
 		cur = (t_tetri *)alst->content;
-		ft_free_tetri(curr);
+		ft_free_tetri(cur);
 		ft_memdel((void **)alst);
-		alst = new;
+		alst = next;
 	}
 }
 
-void	ft_free_tetri(t_tetri *tetri)
+void	ft_free_tetri(t_tetri *atetri)
 {
 	char i;
 
-	if (t_tetri == NULL)
-		return (0);
+	i = 0;
+	ft_free_tab_2d(atetri->tetri, (int)atetri->height);
+	ft_memdel((void **)&(atetri->tetri));
+	ft_memdel((void **)&(atetri->height));
+	ft_memdel((void **)&(atetri->width));
+	ft_memdel((void **)&(atetri));
+}
+
+void	ft_free_tab_2d(char **tab, int height)
+{
+	int i;
+
 	i = 0;
 	while (i < height)
 	{
-		ft_memdel((void **)&(tetri->tetri[i]));
+		ft_memdel((void **)&tab[i]);
 		i++;
 	}
-	ft_memdel((void **)&(tetri->tetri));
-	ft_memdel((void **)&(tetri->height));
-	ft_memdel((void **)&(tetri->width));
-	ft_memdel((void **)&(tetri));
+	free(tab);
 }
 
-t_tetri		ft_new_tetri(char **tetri, char height, char width)
+t_tetri		*ft_new_tetri(char **tetri, char height, char width)
 {
-	t_tetri new;
+	t_tetri *new;
 
-	new = (t_tetri)malloc(sizeof(t_tetri));
-	if (new == NULL)
-		return (-1);
+	new = (t_tetri *)ft_memalloc(sizeof(t_tetri));
 	new->height = height;
 	new->width = width;
 	new->tetri = tetri;
