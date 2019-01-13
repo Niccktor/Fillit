411803970230848d5b299d2e5a208819538ebe52
Thomas BEGUIN <tbeguin@e2r3p20.42.fr>
2019-01-13 20:51:18 +0100

    oust com

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tbeguin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/06 12:47:34 by tbeguin           #+#    #+#             */
-/*   Updated: 2019/01/12 20:37:31 by tbeguin          ###   ########.fr       */
+/*   Updated: 2019/01/13 20:50:31 by tbeguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int		main(int argc, char **argv)
 {
 	t_list	*alst;
+
 	if (argc != 2)
 	{
 		ft_putstr("usage: fillit input_file\n");
@@ -26,83 +27,6 @@ int		main(int argc, char **argv)
 		return (-1);
 	}
 	ft_solve(alst);
-	ft_free_list(alst);
+	sleep(20);
 	return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-int		main(void)		//test leaks tetris
-{
-	char		**map;
-	t_tetri		*atetri;
-
-	map = ft_new_tab_2d(2, 3);
-	atetri = ft_new_tetri(map, 2, 3);
-	ft_free_tetri(atetri);
-	
-	map = ft_new_tab_2d(2, 3);
-	atetri = ft_new_tetri(map, 2, 3);
-	ft_putstr("pas de leaks \n");
-	sleep(5);
-	ft_free_tetri(atetri);
-
-	map = ft_new_tab_2d(2, 3);
-	atetri = ft_new_tetri(map, 2, 3);
-	map = ft_new_tab_2d(2, 3);
-	atetri = ft_new_tetri(map, 2, 3);
-	ft_putstr("leaks !\n");
-	sleep(5);
-	return (0);
-}*/
-
-/*						//test leaks tab2d
-int		main(void)
-{
-	char	**tetris;
-
-	tetris = ft_new_tab_2d(2, 3);
-	ft_free_tab_2d(tetris, 2);
-	tetris = ft_new_tab_2d(2, 3);
-	ft_putstr("pas de leaks \n");
-	sleep(5);
-	ft_free_tab_2d(tetris, 2);
-	tetris = ft_new_tab_2d(2, 3);
-	tetris = ft_new_tab_2d(2, 3);
-	ft_putstr("leaks !\n");
-	sleep(5);
-}
-*/
-/* 						//	test leaks t_map
-int		main(void)
-{
-	t_map	*amap;
-
-	amap = ft_new_map(10);
-	ft_free_map(amap);
-	amap = ft_new_map(10);
-	ft_putstr("pas de leaks \n");
-	sleep(5);
-	ft_free_map(amap);
-	amap = ft_new_map(10);
-	amap = ft_new_map(10);
-	ft_putstr("leaks !\n");
-	sleep(5);
-
-	return (0);
-}*/
