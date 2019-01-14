37c058ac0335f82eac2ae697e3227ba53c913f38
Thomas BEGUIN <tbeguin@e2r8p1.42.fr>
2019-01-14 14:06:41 +0100

    mise a la norme 42

--- src/util.c
+++ src/util.c
@@ -6,13 +6,13 @@
 /*   By: tbeguin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/06 14:36:52 by tbeguin           #+#    #+#             */
-/*   Updated: 2019/01/14 13:22:08 by tbeguin          ###   ########.fr       */
+/*   Updated: 2019/01/14 14:06:25 by tbeguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fillit.h"
 
-void	ft_free_list(t_list *alst)
+t_list	*ft_free_list(t_list *alst)
 {
 	t_tetri *cur;
 	t_list	*next;
@@ -22,10 +22,10 @@ void	ft_free_list(t_list *alst)
 		next = alst->next;
 		cur = (t_tetri *)alst->content;
 		ft_free_tetri(cur);
-//		free(&(alst->content));            	    // ??
 		ft_memdel((void **)alst);
 		alst = next;
 	}
+	return (NULL);
 }
 
 void	ft_free_tetri(t_tetri *atetri)
@@ -39,10 +39,6 @@ void	ft_free_tetri(t_tetri *atetri)
 		i++;
 	}
 	ft_memdel((void **)&(atetri->tetri));
-//	free (atetri);
-//	free(&(atetri));					//		??
-//	ft_memdel((void **)&(atetri));		//		??
-//	ft_memdel((void **)atetri);			//
 }
 
 void	ft_free_map(t_map *amap)
