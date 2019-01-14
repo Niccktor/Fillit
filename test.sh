ea4eac1aaed9d377e4484e420ff00ac42f8e39bd
Thomas BEGUIN <tbeguin@e2r10p10.42.fr>
2019-01-14 13:39:59 +0100

    modif fichier test

--- test.sh
+++ test.sh
@@ -61,10 +61,76 @@ if [ "$1" = "1" -o "$1" = "3" ] ; then
 	read
 	clear
 
-	echo "cat valid_7"
-	cat ./test_files/correct/valid_7
-	echo "Test valid_7"
-	./fillit test_files/correct/valid_7
+	echo "cat valid_8"
+	cat ./test_files/correct/valid_8
+	echo "Test valid_8"
+	./fillit test_files/correct/valid_8
+
+	echo "cat valid_9"
+	cat ./test_files/correct/valid_9
+	echo "Test valid_9"
+	./fillit test_files/correct/valid_9
+
+
+	echo "cat valid_10"
+	cat ./test_files/correct/valid_10
+	echo "Test valid_10"
+	./fillit test_files/correct/valid_10
+
+	echo "cat valid_11"
+	cat ./test_files/correct/valid_11
+	echo "Test valid_11"
+	./fillit test_files/correct/valid_11
+
+	echo "cat valid_12"
+	cat ./test_files/correct/valid_12
+	echo "Test valid_12"
+	./fillit test_files/correct/valid_12
+
+	echo "cat valid_13"
+	cat ./test_files/correct/valid_13
+	echo "Test valid_13"
+	./fillit test_files/correct/valid_13
+
+	echo "cat valid_14"
+	cat ./test_files/correct/valid_14
+	echo "Test valid_14"
+	./fillit test_files/correct/valid_14
+
+	echo "cat valid_15"
+	cat ./test_files/correct/valid_15
+	echo "Test valid_15"
+	./fillit test_files/correct/valid_15
+
+	echo "cat valid_16"
+	cat ./test_files/correct/valid_16
+	echo "Test valid_16"
+	./fillit test_files/correct/valid_16
+
+	echo "cat valid_17"
+	cat ./test_files/correct/valid_17
+	echo "Test valid_17"
+	./fillit test_files/correct/valid_17
+
+	echo "cat valid_18"
+	cat ./test_files/correct/valid_18
+	echo "Test valid_18"
+	./fillit test_files/correct/valid_18
+
+	echo "cat valid_19"
+	cat ./test_files/correct/valid_19
+	echo "Test valid_19"
+	./fillit test_files/correct/valid_19
+
+	echo "cat valid_20"
+	cat ./test_files/correct/valid_20
+	echo "Test valid_20"
+	./fillit test_files/correct/valid_20
+
+	echo "cat valid_21"
+	cat ./test_files/correct/valid_21
+	echo "Test valid_21"
+	./fillit test_files/correct/valid_21
 fi
 clear
 if [ "$1" = "2" -o "$1" = "3" ] ; then
@@ -121,4 +187,78 @@ if [ "$1" = "2" -o "$1" = "3" ] ; then
 	cat ./test_files/error/error_7
 	echo "Test error_7"
 	./fillit test_files/error/error_7
+
+	echo "cat error_8"
+	cat ./test_files/error/error_8
+	echo "Test error_8"
+	./fillit test_files/error/error_8
+
+	echo "cat error_9"
+	cat ./test_files/error/error_9
+	echo "Test error_9"
+	./fillit test_files/error/error_9
+
+	echo "cat error_10"
+	cat ./test_files/error/error_10
+	echo "Test error_10"
+	./fillit test_files/error/error_10
+
+	echo "cat error_11"
+	cat ./test_files/error/error_11
+	echo "Test error_11"
+	./fillit test_files/error/error_11
+
+	echo "cat error_12"
+	cat ./test_files/error/error_12
+	echo "Test error_12"
+	./fillit test_files/error/error_12
+
+	echo "cat error_13"
+	cat ./test_files/error/error_13
+	echo "Test error_13"
+	./fillit test_files/error/error_13
+
+	echo "cat error_14"
+	cat ./test_files/error/error_14
+	echo "Test error_14"
+	./fillit test_files/error/error_14
+
+	echo "cat error_15"
+	cat ./test_files/error/error_15
+	echo "Test error_16"
+	./fillit test_files/error/error_16
+
+	echo "cat error_17"
+	cat ./test_files/error/error_17
+	echo "Test error_17"
+	./fillit test_files/error/error_17
+
+	echo "cat error_18"
+	cat ./test_files/error/error_7
+	echo "Test error_7"
+	./fillit test_files/error/error_7
+
+
+	echo "cat error_7"
+	cat ./test_files/error/error_7
+	echo "Test error_7"
+	./fillit test_files/error/error_7
+
+
+
+
+	echo "cat error_7"
+	cat ./test_files/error/error_7
+	echo "Test error_7"
+	./fillit test_files/error/error_7
+
+
+
+
+
+
+
+
+
+
 fi
